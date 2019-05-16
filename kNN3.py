ad8771c452b9a16ef7dbbbb3b591e4dfc1707697
wangweiya <sa517370@mail.ustc.edu.cn>
2019-05-16 16:48:30 +0800

    norm data 1.0

---
+++ kNN3.py
@@ -0,0 +1,36 @@
+from numpy import *
+import operator
+from os import listdir
+import matplotlib
+import matplotlib.pyplot as plt
+
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())         #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    return normDataSet, ranges, minVals
+
+datingDataMat, datingLabels = file2matrix("/Users/wangweiya/desktop/ch2/datingTestSet2.txt")
+
+normMat, ranges, minVals = autoNorm(datingDataMat)
+
+print normMat, ranges, minVals
