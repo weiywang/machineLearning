e0788bd472f0df3934e29a346d319188717eedd0
wangweiya <sa517370@mail.ustc.edu.cn>
2019-06-02 22:55:37 +0800

    PCA 1.0

---
+++ 12_PCA/1_meanPca.py
@@ -0,0 +1,34 @@
+from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
+
+def loadDataSet(fileName, delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.strip().split(delim) for line in fr.readlines()]
+    datArr = [map(float,line) for line in stringArr]
+    return mat(datArr)
+
+def pca(dataMat, topNfeat=9999999):
+    meanVals = mean(dataMat, axis=0)
+    meanRemoved = dataMat - meanVals #remove mean
+    covMat = cov(meanRemoved, rowvar=0)
+    eigVals,eigVects = linalg.eig(mat(covMat))
+    eigValInd = argsort(eigVals)            #sort, sort goes smallest to largest
+    eigValInd = eigValInd[:-(topNfeat+1):-1]  #cut off unwanted dimensions
+    redEigVects = eigVects[:,eigValInd]       #reorganize eig vects largest to smallest
+    lowDDataMat = meanRemoved * redEigVects#transform data into new dimensions
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    return lowDDataMat, reconMat
+
+if __name__ == '__main__':
+    dataMat = loadDataSet('testSet.txt')
+    lowDat, reconMat = pca(dataMat, 1)
+    print lowDat
+    print reconMat
+    print shape(lowDat)
+    print shape(reconMat)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(dataMat[:, 0].flatten().A[0], dataMat[:, 1].flatten().A[0], marker = '^', s = 90)
+    ax.scatter(reconMat[:, 0].flatten().A[0], reconMat[:, 1].flatten().A[0], marker='o', s = 50, c = 'red')
+    plt.show()
